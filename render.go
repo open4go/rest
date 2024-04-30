44879406505da02f48374534e0bcf4c49d4e0bc3
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-05-01 04:09:31 +0800

    add more info for error

--- render.go
+++ render.go
@@ -1,48 +1,81 @@
 package rest
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"github.com/open4go/log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-// MakeResponse 返回数据
-func MakeResponse(c *gin.Context, err error, msg string, result interface{}) {
-	if err != nil {
-		// 操作失败
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": msg,
-			"error":   err.Error(),
-			"status":  "failed",
-		})
-		return
-	} else {
-		switch v := result.(type) {
-		case string:
-			// 创建
-			c.JSON(http.StatusCreated, gin.H{
-				"message": msg,
-				"id":      v,
-				"status":  "success",
-			})
-			c.Writer.Header().Set("TargetId", v)
-			return
-		default:
-			_id := c.Param("_id")
+// Status Define custom type for response status
+type Status string
 
-			// 查询详情
-			if result != nil {
-				// 返回详情
-				c.IndentedJSON(http.StatusOK, v)
-			} else {
-				// 删除/更新/操作成功
-				c.JSON(http.StatusAccepted, gin.H{
-					"message": msg,
-					"id":      _id,
-					"status":  "success",
-				})
-			}
-			c.Writer.Header().Set("TargetId", _id)
-			return
+// Define constants for response statuses
+const (
+	Success Status = "success"
+	Failed  Status = "failed"
+	// Add more statuses as needed
+)
+
+// MakeResponse returns the response.
+func MakeResponse(c *gin.Context, err error, msg string, result interface{}) {
+	t := time.Now().Unix()
+	// 从请求上下文中获取 Trace ID
+	RequestID, exists := c.Get("RequestID")
+	if !exists {
+		// 如果不存在，使用一个默认值
+		RequestID = "UNKNOWN"
+	}
+	// 读取环境变量
+	version := os.Getenv("SERVER_VERSION")
+	if version == "" {
+		version = "-"
+	}
+
+	if err != nil {
+		// Operation failed
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message":    msg,
+			"error":      err.Error(),
+			"status":     Failed,
+			"timestamp":  t,
+			"request_id": RequestID,
+			"version":    version,
+		})
+		log.Log().WithField("content", msg).
+			Error(err)
+		return
+	}
+
+	switch v := result.(type) {
+	case string:
+		// Creation
+		c.JSON(http.StatusCreated, gin.H{
+			"message": msg,
+			"id":      v,
+			"status":  Success,
+		})
+		c.Writer.Header().Set("TargetId", v)
+		c.Request = c.Request.WithContext(context.WithValue(c.Request.Context(), "TargetId", v))
+	default:
+		_id := c.Param("_id")
+
+		if result != nil {
+			// 如果是请求数据详情则直接返回传入的数据结构本身
+			// Return details
+			c.IndentedJSON(http.StatusOK, v)
+		} else {
+			// Deletion/Update/Operation success
+			c.JSON(http.StatusAccepted, gin.H{
+				"message": msg,
+				"id":      _id,
+				"status":  Success,
+			})
 		}
+		c.Writer.Header().Set("TargetId", _id)
+		c.Request = c.Request.WithContext(context.WithValue(c.Request.Context(), "TargetId", _id))
 	}
 }
